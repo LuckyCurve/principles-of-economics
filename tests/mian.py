01c3d6d297aa184e89cd5611d8a02facb1eab345
Lucky Curve <luckycurvec@gmail.com>
2025-05-05 13:22:01 +0800

    加入分红数据，提升测试数据准确性

--- tests/mian.py
+++ tests/mian.py
@@ -8,14 +8,14 @@ CSV_DIR = "data/csv"
 
 def nasdaq_calculate_earn_rate(month: int) -> float:
     monthly_df = pandas.read_csv(os.path.join(CSV_DIR, "nasdaq_monthly_change.csv"))
-    number = (10000 / (monthly_df.tail(month)["^IXIC"] * (1 - 0.0015))).sum()
+    number = (10000 / (monthly_df.tail(month)["^IXIC"] * (1 - 0.0015 + 0.0062))).sum()
     last_gspc_value = monthly_df.tail(1)["^IXIC"].values[0]
     return (last_gspc_value * number - month * 10000) / (month * 10000)
 
 
 def sp500_calculate_earn_rate(month: int) -> float:
     monthly_df = pandas.read_csv(os.path.join(CSV_DIR, "sp500_monthly_change.csv"))
-    number = (10000 / (monthly_df.tail(month)["^GSPC"]) * (1 - 0.0002)).sum()
+    number = (10000 / (monthly_df.tail(month)["^GSPC"]) * (1 - 0.0002 + 0.0134)).sum()
     last_gspc_value = monthly_df.tail(1)["^GSPC"].values[0]
     return (last_gspc_value * number - month * 10000) / (month * 10000)
 
